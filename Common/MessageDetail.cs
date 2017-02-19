0f5efa58949d72634d71423d7fc5c473f59edab5
Mohammed Salah <msalah89@outlook.com>
2017-02-19 18:10:57 +0200

    first commit

---
+++ Common/MessageDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRChat.Common
+{
+    public class MessageDetail
+    {
+         public string UserName { get; set; }
+
+        public string Message { get; set; }
+    
+    }
+
+     
+}
